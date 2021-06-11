e0936591d433b66667132493da105dfead95098b
Arash Zaheri <arashzaheri@gmail.com>
2021-06-11 11:41:46 -0500

    Part 2 Finished

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #opens the table and creates a variable for it to be accessed through python.
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #Defines the function and sets the parameter.
+    for line in log_file: #Loops through the file and assigns the name line to each row.
+        line = line.rstrip() #Removes any blank in the end of the string
+        day = line[0:3] #targets the index value of 0 to 3 and assigned it the day variable.
+        if day == "Mon": # if the day value is equal to "Tue"/"Mon"
+            print(line)  # then it will print the line to console.
 
 
-sales_reports(log_file)
+sales_reports(log_file) #Calls the function with the server 01 table being opened.
+
